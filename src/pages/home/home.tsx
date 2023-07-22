e66a71576b5a8c7c779a0a6224d5c8a2bea7df79
Greg Skinner <gregory.s.skinner@bath.edu>
2023-07-22 15:39:16 +0100

    Basic page layout

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 
-import { Button } from '@components/button/button';
-
 import styles from './home.module.scss';
+import { useList } from '../../appProvider';
+import { GroceryDisplay } from '@components/groceryDisplay/groceryDisplay';
 
 export interface IHomeProps {}
 
-export const Home: React.FC<IHomeProps> = () => (
-  <div className={styles.content}>
-    <p>Hello world</p>
-    <Button onClick={() => alert(0)}>Useless button</Button>
-  </div>
-);
+export const Home: React.FC<IHomeProps> = () => {
+  const { list } = useList();
+
+  return (
+    <div className={styles.content}>
+      <div className={styles.header}>
+        <div className={styles.toggle}>Shop</div>
+        <span>SHOPPING LIST</span>
+        <div className={styles.toggle}>Add</div>
+      </div>
+      <div className={styles.list}>
+        {list.map((item) => (
+          <GroceryDisplay key={item._name} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
